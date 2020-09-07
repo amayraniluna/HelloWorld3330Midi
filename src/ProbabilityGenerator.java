b3be37527c603660c9c3a1fa74e895a07d909685
amayraniluna <asluna@smu.edu>
2020-09-06 22:34:25 -0500

    added to train()

--- src/ProbabilityGenerator.java
+++ src/ProbabilityGenerator.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 //Amayrani Luna 
 //09/05/20
-//Description: ******Fill this out******
+//Description: taking in notes to train probability generator and using those notes to generate a new melody in real-time.
 
 public class ProbabilityGenerator<T> {
 	
@@ -12,13 +12,32 @@ public class ProbabilityGenerator<T> {
 	
 	ProbabilityGenerator(){
 		alphabet = new ArrayList<T>();
-		alphabet_counts = new ArrayList<Integer>();
+		alphabet_counts = new ArrayList<Integer>(1);
 
 	}
 	
 //it is training probability generator with new data 
 	void train(ArrayList<T> newTokens){
-		//code the training
+		//filling in alphabet + alphabet_count list
+		//************************************
+		//*******LEFT OFF RIGHT HERE**********
+		//************************************
+		
+		for(int i = 0; i < newTokens.size() ; i++){
+			for(int j = 0; j < alphabet.size(); j++){
+				
+				if((newTokens.get(i)).equals(alphabet.get(j))){
+						//adds 1 to alphabet_count
+						int newCount = alphabet_counts.get(j) + 1;
+						alphabet_counts.set(j, newCount);
+				}
+					else {
+						alphabet.add(newTokens.get(i));
+						alphabet_counts.add(1);
+					}
+			}
+				
+		}
 	}
 	
 	
